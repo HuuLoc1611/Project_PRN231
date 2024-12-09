9a7a7fda28dca9e6d14cb78f33f7d2b1a6db2529
Huuloc <ngohuuloc54@gmail.com>
2024-12-09 13:59:29 +0700

    Update export to excel

--- WebClient/Controllers/TransactionController.cs
+++ WebClient/Controllers/TransactionController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using OfficeOpenXml;
 using WebClient.Models;
 using WebClient.Services.VnPay;
 
@@ -81,6 +82,58 @@ namespace WebClient.Controllers
             return View("Manage-Transaction");
         }
 
+       [HttpGet]
+        public async Task<IActionResult> ExportToExcel()
+        {
+            ExcelPackage.LicenseContext = LicenseContext.Commercial;
+
+            List<Transaction>transactions = new List<Transaction>();
+            string url = "https://localhost:5100/api/Transaction";
+
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage res = await client.GetAsync(url))
+                {
+                    using (HttpContent content = res.Content)
+                    {
+                        string data = await content.ReadAsStringAsync();
+                        transactions = JsonConvert.DeserializeObject<List<Transaction>>(data);
+                    }
+                }
+            }
+
+            using (var package = new ExcelPackage())
+            {
+                var worksheet = package.Workbook.Worksheets.Add("Transactions");
+
+                // Adding headers
+                worksheet.Cells[1, 1].Value = "Id";
+                worksheet.Cells[1, 2].Value = "Fullname";
+                worksheet.Cells[1, 3].Value = "Price";
+                worksheet.Cells[1, 4].Value = "Date";
+                // Adding data
+                int row = 2;
+                foreach (var item in transactions)
+                {
+                    worksheet.Cells[row, 1].Value = item.Id;
+                    worksheet.Cells[row, 2].Value = item.Account.FullName; // Image URL as a hyperlink
+                    worksheet.Cells[row, 3].Value = item.Price;
+                    worksheet.Cells[row, 4].Value = item.Date.Value.ToString("dd/MM/yyyy"); ;
+
+                    row++;
+                }
+
+                // Auto-fit columns
+                worksheet.Cells.AutoFitColumns();
+
+                var stream = new MemoryStream();
+                package.SaveAs(stream);
+                stream.Position = 0;
+
+                var fileName = "transaction_data.xlsx";
+                return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
+            }
+        }
 
     }
 }
